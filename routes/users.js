d09a5e47368671cafb52fc1203a7ff5da2650013
christopher-alba <calb200@aucklanduni.ac.nz>
2021-11-14 18:24:14 +1300

    write register endpoint

--- routes/users.js
+++ routes/users.js
@@ -22,4 +22,25 @@ router.post("/login", (req, res) => {
     });
 });
 
+router.post("/register", (req, res) => {
+  register(req.body)
+    .then((response) => {
+      const userObj = response._doc;
+      res
+        .send({
+          username: userObj.username,
+          password: userObj.password,
+          token: response.token,
+        })
+        .status(200);
+    })
+    .catch((err) => {
+      return res
+        .send({
+          error: err,
+        })
+        .status(400);
+    });
+});
+
 module.exports = router;
